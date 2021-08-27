5d52903f3a8f27fd6bf4382e5a88c86ea58c7b32
红血球AE3803 <2544390577@qq.com>
2021-08-27 12:28:10 -0500

    Mojang accont support
    If players are using mojang accont to join server, player can join server without register.

--- config.php
+++ config.php
@@ -13,8 +13,10 @@ $regurl = "https://reg.zhjlfx.cn"; //玩家注册地址
 $publickey = file_get_contents($_SERVER['DOCUMENT_ROOT'] ."/keys/yggdrasil-public-key.pem"); //公钥文件
 $privatekey = file_get_contents($_SERVER['DOCUMENT_ROOT'] ."/keys/yggdrasil-private-key.pem"); //私钥文件
 //外置登录服务器数据库配置//
-$host = 'your mysql host'; //数据库地址
+$host = 'host'; //数据库地址
 $port = 3306; //数据库端口
-$user = 'your mysql user'; //数据库用户名
-$pass = 'your mysql pass'; //数据库密码
-$dbname = 'user database'; //数据库名
+$user = 'user'; //数据库用户名
+$pass = 'pass'; //数据库密码
+$dbname = 'database'; //数据库名
+//正版直接登录支持
+$mojanglogin = true;
